19cea99a4b3e9bd3954ed007ae96d4d81d4395bb
mountaincorgi <jdz.uke2@gmail.com>
2020-09-20 22:51:41 +0100

    start implementing list of options

--- options.js
+++ options.js
@@ -6,8 +6,49 @@ chrome.storage.sync.get(null, function(data) {
     document.getElementById('description-1').setAttribute('value', data.description1);
     document.getElementById('description-2').setAttribute('value', data.description2);
     document.getElementById('description-3').setAttribute('value', data.description3);
-    document.getElementById('block-list-input').setAttribute('value', data.blockList.join(', '));
-    document.getElementById('limit-list-input').setAttribute('value', data.limitList.join(', '));
+
+    let blockUl = document.getElementById('block-list')
+    debugger;
+    for (let i=0; i<data.blockList.length; i++) {
+        let j = document.createElement("li");
+        j.innerHTML = `<input type="text" value="${data.blockList[i]}"><button class="delete">-</button>`;
+        blockUl.appendChild(j);
+    }
+    let k = document.createElement("li");
+    k.innerHTML = `<input type="text"><button class="add">+</button>`;
+    blockUl.appendChild(k);
+
+
+    let limitUl = document.getElementById('limit-list')
+    for (let x=0; x<data.limitList.length; x++) {
+        let y = document.createElement("li");
+        y.innerHTML = `<input type="text" value="${data.limitList[x]}"><button class="delete">-</button>`;
+        limitUl.appendChild(y);
+    }
+    let z = document.createElement("li");
+    z.innerHTML = `<input type="text"><button class="add">+</button>`;
+    limitUl.appendChild(z);
+
+
+    let deleteButtons = document.getElementsByClassName('delete');
+    debugger;
+    for (let b=0; b<deleteButtons.length; b++) {
+        let linsanity = deleteButtons[b];
+        linsanity.addEventListener('click', function() {
+            linsanity.parentElement.remove();
+        });
+    }
+
+
+    let addButtons = document.getElementsByClassName('add');
+    debugger;
+    for (let v=0; v<addButtons.length; v++) {
+        let kobe = addButtons[v];
+        kobe.addEventListener('click', function() {
+            kobe.className = 'delete';
+            kobe.innerHTML = 'do_stuff';
+        });
+    }
 });
 
 
